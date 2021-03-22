5fb2a72a8a2702ce7deb0c2cbe834e4b115d08a6
AjiGaneshaBrahma <73512901+AjiGaneshaBrahma@users.noreply.github.com>
2021-03-22 22:26:48 +0700

    Create biodatasingkat.pas

---
+++ Tugas 1/biodatasingkat.pas
@@ -0,0 +1,26 @@
+{Nama : Aji Ganesha Brahma}
+{NPM  : 20.14.1.0002}
+
+program biodata;
+uses crt;
+
+var
+nama, npm, alamat, ttl, nohp, motto :string;
+
+begin
+write('1. Nama : ');
+readln(nama);
+write('2. NPM : ');
+readln(npm);
+write('3. Alamat : ');
+readln(alamat);
+write('4. TTl : ');
+readln(ttl);
+write('5. No. Hp : ');
+readln(nohp);
+write('6. Motto : ');
+readln(motto);
+
+
+readln;
+end.
