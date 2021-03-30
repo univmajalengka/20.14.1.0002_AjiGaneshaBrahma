cd151d2784c67d20e00bc0ecf55a4ea12865adf2
AjiGaneshaBrahma <73512901+AjiGaneshaBrahma@users.noreply.github.com>
2021-03-30 17:57:23 +0700

    while_do.pas

---
+++ Latihan Baba 4/while_do.pas
@@ -0,0 +1,22 @@
+{nama           : Aji Ganesha Brahma}
+{NPM            :201410002      }
+{nama program   :while_do.pas   }
+
+program while_do;
+uses crt;
+
+var
+        i : integer ;
+
+begin
+clrscr;
+        i :=0;
+        while i < 10 do
+begin
+        writeln('I LOVE YOU');
+        i:= i + 1;
+        end;
+
+        readln;
+
+end.
