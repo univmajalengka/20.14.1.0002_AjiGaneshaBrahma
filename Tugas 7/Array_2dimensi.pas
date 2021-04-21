ad8ad3886a87545cda5f25545234df1c3780e950
AjiGaneshaBrahma <73512901+AjiGaneshaBrahma@users.noreply.github.com>
2021-04-21 18:57:23 +0700

    Array_2dimensi.pas

---
+++ Tugas 7/Array_2dimensi.pas
@@ -0,0 +1,26 @@
+
+{Nama : Aji Ganesha Brahma }
+{NPM  : 201410002 }
+{Nama Program : Array_2dimensi.pas }
+
+program Array_2dimensi;
+uses crt;
+
+var
+nilai : array[0..1,0..1] of integer;
+
+begin
+clrscr;
+
+nilai[0,0] := 3;
+nilai[0,1] := 4;
+nilai[1,0] := 5;
+nilai[1,1] := 6;
+
+write(nilai[0,0],' ');
+writeln(nilai[0,1]);
+write(nilai[1,0],' ');
+writeln(nilai[1,1]);
+readln;
+
+end.
